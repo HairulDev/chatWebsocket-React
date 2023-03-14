1af5f2452401163768027227d89da653df7d7115
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-03-14 17:19:55 +0700

    Update users.js

--- src/actions/users.js
+++ src/actions/users.js
@@ -2,7 +2,7 @@ import { FETCH_ALL, } from '../constants/actionTypes';
 import axios from "axios";
 import env from '../configs/vars';
 
-const API = axios.create({ baseURL: env.reactAppHost });
+const API = axios.create({ baseURL: "http://ec2-175-41-187-69.ap-southeast-1.compute.amazonaws.com:3001" });
 
 export const fetchUser =
   (successCB, failedCB) => async (dispatch) => {
