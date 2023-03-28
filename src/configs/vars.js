67053bffe1cf9f6e866232fe34ac801fccd0a7c9
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-03-28 14:02:36 +0700

    Update vars.js

--- src/configs/vars.js
+++ src/configs/vars.js
@@ -2,9 +2,9 @@
 
 const env = {
   port: 3100,
-  reactAppHost: "http://ec2-175-41-187-69.ap-southeast-1.compute.amazonaws.com:3001",
-  publicUrl: "http://ec2-175-41-187-69.ap-southeast-1.compute.amazonaws.com:3000",
-  reactAppWebSocketHost: "ws://ec2-175-41-187-69.ap-southeast-1.compute.amazonaws.com:3001",
+  reactAppHost: "http://ec2-18-140-62-35.ap-southeast-1.compute.amazonaws.com:3001",
+  publicUrl: "http://ec2-18-140-62-35.ap-southeast-1.compute.amazonaws.com:3000",
+  reactAppWebSocketHost: "ws://ec2-18-140-62-35.ap-southeast-1.compute.amazonaws.com:3001",
 }
 
 export default env;
